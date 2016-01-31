6e0589550017a0e91537c00157ece886736d122d
nw <nwhasd@126.com>
2016-01-31 20:11:47 +0800

    pp

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -2,15 +2,12 @@
 //
 
 #include "stdafx.h"
-#include <iostream>
-using namespace std;
+
 
 int main()
 {
 	int t;
-	cout << "nihao" << endl;
-	int pppt;
-	cout << "hello world" << endl;
+	
     return 0;
 }
 
